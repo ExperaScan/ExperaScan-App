c21c0ca6b6dcf4a1ead4e5461c7acaeab18b3735
Menno van den Ende <mentosmenno2@hotmail.com>
2017-04-11 11:02:59 +0200

    add plugins
    add custom config plugin
    add barcod escanner plugin
    configure custom config plugin

--- ExperaScan-App/platforms/android/platform_www/cordova_plugins.js
+++ ExperaScan-App/platforms/android/platform_www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
         "id": "cordova-plugin-dialogs.notification",
         "file": "plugins/cordova-plugin-dialogs/www/notification.js",
         "pluginId": "cordova-plugin-dialogs",
@@ -14,14 +22,6 @@ module.exports = [
         "pluginId": "cordova-plugin-dialogs",
         "merges": [
             "navigator.notification"
-        ]
-    },
-    {
-        "id": "cordova-plugin-device.device",
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "pluginId": "cordova-plugin-device",
-        "clobbers": [
-            "device"
         ]
     },
     {
@@ -50,17 +50,27 @@ module.exports = [
             "cordova.plugins.notification.local.core",
             "plugin.notification.local.core"
         ]
+    },
+    {
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.2",
-    "cordova-custom-config": "3.1.3",
-    "cordova-plugin-dialogs": "1.3.2",
-    "cordova-plugin-device": "1.1.5",
     "cordova-plugin-app-event": "1.2.0",
-    "de.appplant.cordova.plugin.local-notification": "0.8.4"
+    "cordova-plugin-device": "1.1.5",
+    "cordova-plugin-dialogs": "1.3.2",
+    "cordova-plugin-whitelist": "1.3.2",
+    "de.appplant.cordova.plugin.local-notification": "0.8.4",
+    "cordova-custom-config": "3.1.3",
+    "cordova-plugin-compat": "1.1.0",
+    "phonegap-plugin-barcodescanner": "6.0.6"
 };
 // BOTTOM OF METADATA
 });
--- ExperaScan-App/platforms/browser/www/cordova_plugins.js
+++ ExperaScan-App/platforms/browser/www/cordova_plugins.js
@@ -56,17 +56,32 @@ module.exports = [
             "cordova.plugins.notification.local.core",
             "plugin.notification.local.core"
         ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-barcodescanner/src/browser/BarcodeScannerProxy.js",
+        "id": "phonegap-plugin-barcodescanner.BarcodeScannerProxy",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.3.2",
-    "cordova-custom-config": "3.1.3",
     "cordova-plugin-dialogs": "1.3.2",
     "cordova-plugin-device": "1.1.5",
     "cordova-plugin-app-event": "1.2.0",
-    "de.appplant.cordova.plugin.local-notification": "0.8.4"
+    "de.appplant.cordova.plugin.local-notification": "0.8.4",
+    "cordova-custom-config": "3.1.3",
+    "phonegap-plugin-barcodescanner": "6.0.6"
 }
 // BOTTOM OF METADATA
 });
